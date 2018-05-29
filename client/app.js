ce663cc0ad215b90a7045d8e564c5e0753af60ba
Clark Newell <clarknewell@Clarks-MacBook-Pro.local>
2018-05-28 19:45:15 -0600

    app almost final

---
+++ client/app.js
@@ -0,0 +1,43 @@
+// const cardElement = document.querySelector('#card-element')
+// const paymentForm = document.querySelector('form')
+// const message = document.querySelector('#message')
+// const errors = document.querySelector('#errors')
+
+const stripe = Stripe('pk_test_KTbKaGW7CdETzcGFc6ASKK93')
+const elements = stripe.elements()
+
+const style = {
+    base: {
+        color: '#32325d',
+        fontFamily: "'Helvetica Neue', Helvetica, sans-serif'",
+        fontSmoothing: 'antialiased',
+        fontSize: '16px',
+        '::placeholder': {
+            color: '#aab7c4'
+        }
+    },
+    invalid: {
+        color: '#fa755a',
+        iconColor: '#fa755a'
+    }
+}
+
+const card = elements.create('card', { style: style })
+card.mount('#card-element')
+
+const form = document.getElementById('payment-form')
+
+form.addEventListener('submit', function (event) {
+    event.preventDefault()
+    stripe.createToken(card).then(result => {
+        stripeTokenHandler(result.token, event.target)
+    })
+
+    function stripeTokenHandler(token, form) {
+        const success = document.createElement('h2')
+        const paidAmount = document.querySelector('#amount').value
+
+        success.textContent = `Your payment of ${paidAmount} dollars has been accepted.`
+        form.appendChild(success)
+    }
+})
--- server/index.js
+++ server/index.js
@@ -1,3 +1,4 @@
+const dotenv = require('dotenv').config()
 const express = require('express')
 const bodyParser = require('body-parser')
 const morgan = require('morgan')
@@ -11,47 +12,54 @@ app.use(bodyParser.json())
 app.use(cors())
 
 app.get('/', (req, res) => {
-  res.json('She done already done had hersss')
+      res.json('She done already done had hersss')
 })
 
-app.post('/', (req, res) => {
-  const stripe = require("stripe")(process.env.STRIPE_SECRET)
+app.post('/charge', (req, res) => {
+    const token = req.body.stripeToken
+    charge(
+        req.body.amount * 100,
+        req.body.service,
+        req.body.stripeToken
+      )
 
-// Token is created using Checkout or Elements!
-// Get the payment token ID submitted by the form:
-// Using Express
-// Set your secret key: remember to change this to your live secret key in production
-// See your keys here: https://dashboard.stripe.com/account/apikeys
+    .catch(error => {
+        console.log(error)
+      })
+})
 
-  stripe.charges.create({
-    amount: 999,
-    currency: 'usd',
-    description: 'Example charge',
-    source: request.body.stripeToken
-  }, function (error, charge) {
-    if (error) {
-      response.status(400).send({error: error.message, stack: error.stack})
-    } else {
-      response.send(charge)
-    }
+function charge(amount, service, token) {
+    return new Promise((resolve, reject) => {
+        stripe.charges.create({
+            amount,
+            currency: "usd",
+            description: service,
+            source: token
+        }, (error, charge) => {
+            if (error) {
+                reject(error)
+            } else {
+                resolve(charge)
+            }
+        })
   })
+}
+
+app.use((err, req, res, next) => {
+    res.status(404)
+    const error = new Error('Not Found. 🔍')
+    next(error)
 })
 
-app.use((req, res, next) => {
-  res.status(404)
-  const error = new Error('Not Found. 🔍')
-  next(error)
-})
-
-app.use((error, req, res, next) => {
-  res.status(res.statusCode || 500)
-  res.json({
-    message: error.message,
-    error: error.stack
-  })
+app.use((err, req, res, next) => {
+    res.status(res.statusCode || 500)
+    res.json({
+        message: error.message,
+        error: error.stack
+    })
 })
 
 const port = process.env.PORT || 5000
 app.listen(port, () => {
-  console.log(`Listening on ${port}`)
+    console.log(`Listening on ${port}`)
 })
